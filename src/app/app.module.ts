fe918fa35b38e58a4470e5d7727a4b9d4edc12a7
CRISTHIAN <cristhian19cd@hotmail.com>
2024-02-21 09:25:43 -0500

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,88 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { GoogleSigninComponent } from './google-signin/google-signin.component';
+import { AddUserComponent } from './components/users/add-user/add-user.component';
+
+import {ReactiveFormsModule} from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatNativeDateModule} from '@angular/material/core';
+import {MatTableModule} from '@angular/material/table';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatGridListModule} from '@angular/material/grid-list';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import {MatCardModule} from '@angular/material/card';
+import {MatDividerModule} from '@angular/material/divider';
+
+
+import {
+  SocialLoginModule,
+  SocialAuthServiceConfig,
+  GoogleLoginProvider,
+} from '@abacritt/angularx-social-login';
+import { HttpClientModule } from '@angular/common/http';
+import { UsersListComponent } from './components/users/users-list/users-list.component';
+import { LoginComponent } from './components/users/login/login.component';
+
+
+
+@NgModule({
+  declarations: [AppComponent, GoogleSigninComponent, UsersListComponent,AddUserComponent,LoginComponent ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    MatNativeDateModule,
+    MatDatepickerModule,
+    MatDividerModule,
+    MatSnackBarModule,
+    MatIconModule,
+    MatDialogModule,
+    MatGridListModule,
+    MatFormFieldModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatCardModule,
+    SocialLoginModule
+    ],
+  providers:[
+    {
+      provide: 'SocialAuthServiceConfig',
+      useValue: {
+        autoLogin: false,
+        providers: [
+          {
+            id: GoogleLoginProvider.PROVIDER_ID,
+            provider: new GoogleLoginProvider('162022528095-s5kjo980evm1u9ikbiljoolj1l4h1767.apps.googleusercontent.com', {
+
+              scopes: 'openid profile email',
+            }),
+          },
+        ],
+        onError: (err) => {
+          console.error(err);
+        },
+      } as SocialAuthServiceConfig,
+
+    },
+  ],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ src/app/components/users/login/login.component.ts
@@ -0,0 +1,104 @@
+import { Component, OnInit, OnDestroy, HostListener } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Login } from 'src/app/models/login_model';
+import { Response } from 'src/app/models/reponse_model';
+import { Subscription } from 'rxjs';
+import { SecurityService } from 'src/app/services/security.service';
+import { UsersService } from 'src/app/services/users.service';
+import { ErrorStateMatcher1 } from 'src/app/error-state-matcher1';
+import { SocialAuthService, SocialUser } from '@abacritt/angularx-social-login';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html'
+})
+export class LoginComponent implements OnInit, OnDestroy {
+  formLogin: FormGroup;
+  subRef$?: Subscription;
+  matcher = new ErrorStateMatcher1();
+  scrHeight: any;
+  scrWidth: any;
+  authSubscription!: Subscription;
+  user1:SocialUser | null=null;
+  loggedIn = false;
+
+  // HostListener: https://angular.io/api/core/HostListener
+  @HostListener('window:resize', ['$event'])
+  getScreenSize() {
+    this.scrHeight = window.innerHeight;
+    this.scrWidth = window.innerWidth;
+    console.log(this.scrHeight, this.scrWidth);
+  }
+
+  constructor(
+    formBuilder: FormBuilder,
+    private router: Router,
+    private authService: SocialAuthService,
+    private dataService: UsersService,
+    private securityService: SecurityService
+  ) {
+    this.getScreenSize();
+    this.securityService.LogOff();
+
+    this.formLogin = formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  ngOnInit() {
+    this.authSubscription = this.authService.authState.subscribe((user) => {
+      localStorage.setItem('token',user.idToken)
+      console.log('user10', user.idToken);
+      console.log("token "+localStorage.getItem('token'));
+      if (user.idToken == null) {
+        console.log("no esta logueado");
+      }else{
+
+        this.router.navigate(['']);
+      }
+
+    });
+
+  }
+
+Login() {
+    const userLogin: Login = {
+      username: this.formLogin.value.username,
+      password: this.formLogin.value.password,
+    };
+
+    console.log(userLogin);
+    this.subRef$ = this.dataService.login<Response>(userLogin)
+      .subscribe({
+        next: (res) => {
+          const token = res.body!.response;
+          console.log('token', token);
+          //this.securityService.SetAuthData(token);
+          sessionStorage.setItem('token', token);
+           this.router.navigate(['']);
+        },
+        error:(response) =>{
+          console.log(response);
+        },
+});
+  }
+
+  hasError(nombreControl: string, validacion: string) {
+    const control = this.formLogin.get(nombreControl);
+    return control?.hasError(validacion);
+  }
+
+  ngOnDestroy() {
+    if (this.subRef$) {
+      this.subRef$.unsubscribe();
+    }
+    this.authSubscription.unsubscribe();
+  }
+
+  googleSignin(googleWrapper: any) {
+    googleWrapper.click();
+  }
+
+}
